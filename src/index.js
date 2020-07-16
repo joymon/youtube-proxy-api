17d0e9a4e24c9a72e35e4a39156373e34a17f62d
Joy George Kunjikkuru <joymon@gmail.com>
2020-07-16 15:37:21 -0400

    Added Youtube API and restricted to 3 channels

--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
-const express = require( "express" );
+const express = require("express");
 const app = express();
+var http = require('http');
+var request = require('request');
+
 const port = process.env.PORT || 8080; // default port to listen
+const YTAPIKey = process.env.YTAPIKey
+
+const allowedChannels = ["UC1D3Fy0L0aoI0OfUEZnWy5A", //JJP
+    "UCzYqSifCwLYpmgRxvNo7BzA", //J3D
+    "UC78wYrq_keVaDV8STReHRxg"];// JVC
 
 // define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    res.send( "Hello world!" );
-} );
-app.get( "/api/envs/:keyName", ( req, res ) => {
-    res.send( "Hello world!"+ process.env[req.params.keyName] );
-} );
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+});
+app.get("/api/envs/:keyName", (req, res) => {
+    res.send("Hello world!" + process.env[req.params.keyName]);
+});
+getConfigValueFromEnv = function (key) {
+    "YTAPIKey"
+}
+app.get("/api/channels/:channelId/videos", async (req, res) => {
+
+    const channelId = req.params.channelId;
+    if (allowedChannels.includes(channelId)) {
+        let url = `https://www.googleapis.com/youtube/v3/search?maxResults=3&type=video&channelId=${channelId}&key=${process.env.YTAPIKey}`;
+        request.get({ url: url, json: true }, (error, response, body) => {
+            //console.log(response);
+            let items = response.body.items.map(item => {
+                return { "id": item.id.videoId };
+            });
+            res.send(items);
+        });
+    }
+    else {
+        res.status(422).json({status:422,message:"This API doesn't support all the channels due to API limits."});
+    }
+    // res.send([{
+    //     "title": url,
+    //     "description": "Test desc",
+    //     "id": "345"
+    // }]);
+});
 // start the Express server
-app.listen( port, () => {
+app.listen(port, () => {
     console.log(process.env.PORT);
-    console.log( `server started at http://localhost:${ port }` );
-} );
+    console.log(`server started at http://localhost:${port}`);
+});
