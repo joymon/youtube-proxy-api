144556bdcd3059428a12a954273948b322121f15
Joy George Kunjikkuru <joymon@gmail.com>
2020-07-16 12:45:01 -0400

    Port adjustment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require( "express" );
 const app = express();
-const port = process.env.port || 8080; // default port to listen
+const port = process.env.PORT | 8080; // default port to listen
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
@@ -9,5 +9,6 @@ app.get( "/", ( req, res ) => {
 
 // start the Express server
 app.listen( port, () => {
+    console.log(process.env.PORT);
     console.log( `server started at http://localhost:${ port }` );
 } );
