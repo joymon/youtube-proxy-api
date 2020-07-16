cb0b51d23cc9600705da685e73725eba761d765d
Joy George Kunjikkuru <joymon@gmail.com>
2020-07-16 16:55:31 -0400

    Added more props

--- src/index.js
+++ src/index.js
@@ -22,17 +22,22 @@ app.get("/api/channels/:channelId/videos", async (req, res) => {
 
     const channelId = req.params.channelId;
     if (allowedChannels.includes(channelId)) {
-        let url = `https://www.googleapis.com/youtube/v3/search?maxResults=3&order=date&type=video&channelId=${channelId}&key=${process.env.YTAPIKey}`;
+        let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=3&order=date&type=video&channelId=${channelId}&key=${process.env.YTAPIKey}`;
         request.get({ url: url, json: true }, (error, response, body) => {
             //console.log(response);
             let items = response.body.items.map(item => {
-                return { "id": item.id.videoId };
+                return {
+                    "id": item.id.videoId,
+                    "title":item.snippet.title,
+                    "description":item.snippet.description,
+                    "publishedAt":item.snippet.publishedAt
+                };
             });
             res.send(items);
         });
     }
     else {
-        res.status(422).json({status:422,message:"This API doesn't support all the channels due to API limits."});
+        res.status(422).json({ status: 422, message: "This API doesn't support all the channels due to API limits." });
     }
     // future Format [{
     //     "title": url,
